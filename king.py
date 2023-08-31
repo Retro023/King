2a6ae65042aeee631d111f13964c6a65a3bed0bf
Mute <107277836+Retro023@users.noreply.github.com>
2023-08-31 22:10:55 +0100

    Create king.py

---
+++ king.py
@@ -0,0 +1,15 @@
+#!/usr/bin python3
+# auto add your username to king file
+
+def search_str(file_path, word):
+        with open(file_path, 'r') as file:
+                content = file.read()
+        if word in content:
+                print("name is still in king")
+
+        else:
+                fp = open('test.txt', 'w')
+                fp.write('Retro0989')
+                fp.close()
+
+search_str(r'/root/king.txt, 'USERNAME')
